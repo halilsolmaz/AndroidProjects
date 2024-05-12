c7f1ee7aa7c75684e3ac4c6b6a3e468a5f97541d
halilsolmaz <halilsolmaz1995@gmail.com>
2024-05-12 22:36:48 +0300

    002-ActivityLifeCycle

--- src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
+++ src/Projects/Android/002-ActivityLifeCycle/app/src/main/java/org/csystem/android/app/activitylifecycle/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.csystem.android.app.activitylifecycle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Toast.makeText(this, R.string.on_create_message, Toast.LENGTH_SHORT)
+            .show() // this burada MainActivite türünden bir referanstır.(upcasting)
+    }
+
+    override fun onStart() {
+        Toast.makeText(this, R.string.on_start_message, Toast.LENGTH_SHORT).show()
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Toast.makeText(this, R.string.on_resume_message, Toast.LENGTH_SHORT).show()
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Toast.makeText(this, R.string.on_restart_message, Toast.LENGTH_SHORT).show()
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Toast.makeText(this, R.string.on_pause_message, Toast.LENGTH_SHORT).show()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Toast.makeText(this, R.string.on_stop_message, Toast.LENGTH_SHORT).show()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, R.string.on_destroy_message, Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
